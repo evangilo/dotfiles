07e2e4d7273c595db5b377668e9c3f7b92cea5be
Evangilo Morais <evangilo@evolux.net.br>
2018-02-08 16:02:10 -0300

    refactoring install.sh

--- install.sh
+++ install.sh
@@ -4,33 +4,57 @@ if [ -z "$DOTFILES" ]; then
   DOTFILES=~/.dotfiles
 fi
 
-if [ -d "$DOTFILES" ]; then
-  env git --work-tree=$DOTFILES --git-dir=$DOTFILES/.git pull origin master || {
-     printf "Error: git pull of dotfiles repo failed.\n"
-     exit 1
-  }
-else
-  env git clone https://github.com/evangilo/dotfiles.git $DOTFILES || {
-    printf "Error: git clone of dotfiles repo failed.\n"
-    exit 1
-  }
-fi
-
-function link {
-  ln -sf $DOTFILES/$1 $HOME/$2
+function pull_repo {
+  if [ -d "$DOTFILES" ]; then
+    env git --work-tree=$DOTFILES --git-dir=$DOTFILES/.git pull origin master || {
+      printf "Error: git pull of dotfiles repo failed.\n"
+      exit 1
+    }
+  else
+    env git clone https://github.com/evangilo/dotfiles.git $DOTFILES || {
+      printf "Error: git clone of dotfiles repo failed.\n"
+      exit 1
+    }
+ fi
 }
 
-link "git/gitconfig" ".gitconfig"
-link "git/gitignore" ".gitignore"
-link "hg/hgrc" ".hgrc"
-link "hg/hgignore" ".hgignore"
-link "vim/vimrc" ".vimrc"
-link "emacs/emacs" ".emacs"
-link "zsh/zshrc" ".zshrc"
-link "zsh/aliases" ".aliases"
-link "zsh/profile" ".profile"
+function create_links {
+  ln -srf git/gitconfig ~/.gitconfig
+  ln -srf git/gitignore ~/.gitignore
+  ln -srf hg/hgrc ~/.hgrc
+  ln -srf hg/hgignore ~/.hgignore
+  ln -srf vim/vimrc ~/.vimrc
+  ln -srf emacs/emacs ~/.emacs
+  ln -srf zsh/zshrc ~/.zshrc
+  ln -srf zsh/aliases ~/.aliases
+  ln -srf zsh/profile ~/.profile
+}
 
-source install_deps.sh
+function installer {
+  if which pacman &> /dev/null; then
+    sudo pacman -S $@;
+  elif which apt-get &> /dev/null; then
+    sudo apt-get install $@;
+  elif which brew &> /dev/null; then
+    brew install $@;
+  fi
+}
+
+function install_programs {
+  installer emacs vim mercurial zsh ncdu
+}
+
+function configure_vim {
+  if [[ ! -d "~/.vim/bundle/Vundle.vim" ]]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  fi
+  vim +PluginInstall +qall
+}
+
+pull_repo
+create_links
+install_programs
+configure_vim
 
 printf "Done!\n"
 

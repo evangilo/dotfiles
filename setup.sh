47e014310eadbd3b60979213f8dbffb5297bb19c
Evangilo Morais <evangilo@evolux.net.br>
2020-04-12 18:46:12 -0300

    cloning dotfiles repository

--- setup.sh
+++ setup.sh
@@ -5,5 +5,11 @@ set -ex
 if [ -z "$DOTFILES" ]; then
   DOTFILES=~/.dotfiles
 fi
+
+if [ -d "$DOTFILES" ]; then
+  git --work-tree=$DOTFILES --git-dir=$DOTFILES/.git pull origin master
+else
+  git clone https://github.com/evangilo/dotfiles.git $DOTFILES
+fi
   
 find  -L $DOTFILES -type f  -name "install.sh" -exec /bin/bash {} \;

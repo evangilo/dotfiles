ceb623f444018365caff76e2d30da33eaef8b1ab
Evangilo Morais <evangilo@evolux.net.br>
2022-03-27 09:45:25 -0300

    install tmux

--- misc/install.sh
+++ misc/install.sh
@@ -3,7 +3,7 @@
 DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd )"
 
 function install {
-  bash $DIR/../pkg_install.sh fzf ripgrep ncdu direnv
+  bash $DIR/../pkg_install.sh fzf ripgrep ncdu direnv tmux
   sudo bash $DIR/fix_cedilla.sh
   sudo bash $DIR/fix_keychron_function_keys.sh
 }
